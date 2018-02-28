0a24654a645b69f2113c73dcb05918681f7c51db
mahimamangal <31283612+mahimamangal@users.noreply.github.com>
2018-02-28 18:35:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function(req,res)
 {
-    res.send('Article 1');
+     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+
 });
 
 app.get('/article-two',function(req,res)
